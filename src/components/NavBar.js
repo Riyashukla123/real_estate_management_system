54996c2f0b105730091c0ee4bb6f64db23979b5d
Riyashukla123 <riya70077@gmail.com>
2025-08-05 23:59:40 +0530

    style improvement

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,47 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { AuthContext } from './AuthContext';
 import { Link, useLocation }  from 'react-router-dom';
+import { FaBars} from 'react-icons/fa';
 
 function NavBar() {
    
    const location = useLocation();
    const currentPath = location.pathname;
    const {user}= useContext(AuthContext);
+   const [menuOpen, setMenuOpen] = useState(false);
   return (
-    
-    <div className={`flex z-50 flex-row justify-between  fixed w-screen h-[70px] items-center  py-4 font-sans px-4 ${currentPath==='/login'?'bg-[#F9F4ED]' : 'bg-white'}`}>
-      <p>Buy Property</p>
-      <p>Sell Property</p>
-      <Link to='/'>
-      <p className="text-xl font-archivo tracking-wider text-gray-900">Shukla's</p>
-      </Link>
-        <p>User's Guide</p>
-      {user ? (
-        <Link to="/dashboard"><p>Account</p></Link>
-      ) : (
-        <Link to="/login"><p>Login</p></Link>
-      )}
-    </div>
+    <>
+      <div className={`hidden md:flex z-50 flex-row justify-between  fixed w-screen h-[70px] items-center  py-4 font-sans px-4 md:pr-8 ${currentPath==='/login'?'bg-[#F9F4ED] ' : 'bg-white '} `}>
+        <p>Buy Property</p>
+        <p>Sell Property</p>
+        <Link to='/'>
+        <p className="text-xl font-archivo tracking-wider text-gray-900">Shukla's</p>
+        </Link>
+          <p>User's Guide</p>
+        {user ? (
+          <Link to="/dashboard"><p>Account</p></Link>
+        ) : (
+          <Link to="/login"><p>Login</p></Link>
+        )}
+      </div>
+      <div className='md:hidden'>
+        <span className={` ${currentPath==='/login'?'bg-[#F9F4ED] before:bg-[#ffffff]' : 'bg-white before:bg-[#F9F4ED]'} px-7 py-2 text-2xl font-bold fixed top-4 right-4 z-50 rounded-full shadow-xl  before:w-5 before:h-5 before:absolute before:left-[-5px] before:bottom-[-5px] before:rounded-full before:content-[''] before:shadow-xl `} onClick={()=>setMenuOpen(!menuOpen)}>
+        <FaBars/></span>
+        <div className={`${menuOpen ? 'translate-x-0' : 'translate-x-[110%]'} w-[calc(100vw-20px)] h-[calc(100vh-115px)] fixed top-[70px] left-[10px] bg-white rounded-lg z-50 font-archivo transition-all duration-700 flex flex-col justify-center items-center gap-8 underline text-4xl font-bold`}>
+          <p>Buy Property</p>
+        <p>Sell Property</p>
+        <Link to='/'>
+        <p>Shukla's</p>
+        </Link>
+          <p>User's Guide</p>
+        {user ? (
+          <Link to="/dashboard"><p>Account</p></Link>
+        ) : (
+          <Link to="/login"><p>Login</p></Link>
+        )}
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import house from '../house-ser.jpg'
 import apartment from '../apartment-ser.jpg'
+import chair from '../Images/chair.webp'
+import houseim from '../Images/house.webp'
 
 function Services() {
   return (
-    <div className="flex flex-row gap-[5px] pt-[10px]">
+    <div className="flex flex-col md:flex-row gap-[5px] pt-[10px]">
       <div className=' relative rounded-[15px] w-full aspect-square overflow-hidden'>
          <img src={house} alt="home" className="w-full h-full object-cover"/>
       </div>
@@ -13,15 +15,18 @@ function Services() {
      </div>
       <div className=' flex flex-col relative rounded-[15px] overflow-hidden w-full aspect-square gap-[5px]'>
         <div className=" w-full h-1/2 bg-[#F9F4ED] relative"> 
+          <img src={chair} className='absolute top-0 left-0 h-[80%]' alt='expect better'></img>
           <div className="absolute w-2/3 bottom-3 right-3 text-right">
-            <h1 className="text-black font-bold text-[2vw] pl-auto ">Expect better</h1>
-            <p className="text-[1.3vw]  text-[#B2A58A] wrap-around leading-none">Extraordinary reach + results helping you find your home</p>
+
+            <h1 className="text-black font-bold text-4xl md:text-[2vw] pl-auto ">Expect better</h1>
+            <p className="text-xl md:text-[1.3vw]  text-[#B2A58A] wrap-around leading-none">Extraordinary reach + results helping you find your home</p>
           </div>
         </div>
-        <div className=" w-full h-1/2 bg-[#F9F4ED] relative">
-          <div className="absolute w-2/3 bottom-3 left-3 text-left">
-            <h1 className="text-black font-bold text-[2vw] pl-auto ">Homes that match</h1>
-            <p className="text-[1.3vw]  text-[#B2A58A] wrap-around leading-none">Really executives international, experience excellence for now</p>
+        <div className=" w-full h-1/2 bg-[#F9F4ED] relative overflow-hidden">
+        <img src={houseim} className='absolute bottom-0 right-0 h-full opacity-80' alt='expect better'></img>
+          <div className="absolute w-2/3 top-3 left-3 text-left">
+            <h1 className="text-black font-bold text-4xl md:text-[2vw] pl-auto ">Homes that match</h1>
+            <p className="text-xl md:text-[1.3vw]  text-[#B2A58A] wrap-around leading-none">Really executives international, experience excellence for now</p>
           </div>
         </div>
       </div>
